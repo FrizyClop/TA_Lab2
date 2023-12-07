40494ce20775dff0f681bf6194da79024c2f0f46
FrizyClop <wifikin@mail.ru>
2023-12-07 18:54:22 +0300

    Небольшие исправления 2

--- Lab2.py
+++ Lab2.py
@@ -1,7 +1,7 @@
 import ClassPerson
 
 first = ClassPerson.Person("Максим","Лютый",2)
-second = ClassPerson.Person("Алсексей", "Леонов",3)
+second = ClassPerson.Person("Алексей", "Леонов",3)
 third = ClassPerson.Person("Дмитрий","Чесноков",4)
 
 print(first.MainInformation())
